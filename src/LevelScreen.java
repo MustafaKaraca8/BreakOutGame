0a3731114a9091bd6bff4306cfce17c7c1d7efb4
MustafaKaraca8 <mstafakarca@gmail.com>
2023-12-21 19:41:53 +0300

    Güncel

---
+++ src/LevelScreen.java
@@ -0,0 +1,84 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class LevelScreen extends JPanel {
+
+    public LevelScreen() {
+
+        setLayout(new GridBagLayout());
+        setPreferredSize(new Dimension(Commons.WIDTH, Commons.HEIGHT));
+
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0, 5, 100, 0); // 100 x üst boşluk
+
+
+        // Oyun İsmi İçin Konum Ayarlaması
+        ImageIcon imageIcon = new ImageIcon("src/resources/images/gameName.png");
+        JLabel imageLabel = new JLabel(imageIcon);
+        add(imageLabel, gbc);
+
+
+        gbc.gridy = 1;
+        gbc.insets = new Insets(0, 0, 0, 0);
+        JLabel restartButtonLabel = getRestartButton();
+        add(restartButtonLabel, gbc);
+
+        imageLabel.setPreferredSize(new Dimension(imageIcon.getIconWidth(), imageIcon.getIconHeight()));
+    }
+
+    private JLabel getRestartButton() {
+        ImageIcon startButtonImage = new ImageIcon(Commons.pathOfButton + "restart.png");
+        ImageIcon onStartButtonImage = new ImageIcon(Commons.pathOfButton + "onRestart.png");
+        JLabel startButtonLabel = new JLabel(startButtonImage);
+        startButtonLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+                startButtonLabel.setIcon(onStartButtonImage);
+                // Restart butonun üzerine gelince diğer resim öğeleri haraket ediyordu onu düzeltmek için
+                // set preferred size kullanıldı
+                startButtonLabel.setPreferredSize(new Dimension(startButtonImage.getIconWidth(), startButtonImage.getIconHeight() + 10));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                startButtonLabel.setIcon(startButtonImage);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                var parent = (Breakout) SwingUtilities.getWindowAncestor(LevelScreen.this);
+                parent.cardLayout.show(parent.cardPanel, "game_screen");
+                parent.gameScreen.startGame();
+                parent.gameScreen.startTimer();
+                parent.gameScreen.requestFocusInWindow();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startButtonLabel.setIcon(startButtonImage);
+            }
+        });
+        return startButtonLabel;
+    }
+
+    public void openLevelScreen() {
+        // Bitiş ekranını aç
+        var parent = (Breakout) SwingUtilities.getWindowAncestor(LevelScreen.this);
+        parent.cardLayout.show(parent.cardPanel, "level_screen");
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Arka plan resmini çiz
+        ImageIcon background = new ImageIcon("src/resources/images/background.png");
+        Image backgroundImage = background.getImage();
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(),this);
+}
+}
