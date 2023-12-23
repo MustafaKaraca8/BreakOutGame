bb7077a5d5591669bc5a7af3363ceb11b096295e
MustafaKaraca8 <mstafakarca@gmail.com>
2023-12-23 19:55:35 +0300

    Güncel

---
+++ src/screens/InfoScreen.java
@@ -0,0 +1,75 @@
+package screens;
+import audio.AudioController;
+import ui.Breakout;
+import utility.Commons;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import static utility.Commons.pathOfAudio;
+import static utility.Helper.where;
+
+public class InfoScreen extends JPanel {
+
+    private AudioController buttonSound = new AudioController();
+
+    public InfoScreen(){
+        where = 2;
+        System.out.println("eÇalıştı");
+        setLayout(new GridBagLayout());
+        setPreferredSize(new Dimension(Commons.WIDTH, Commons.HEIGHT));
+
+
+        // Konum belirlemek için
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(Commons.HEIGHT - 150, Commons.WIDTH - 200, 0, 0);
+        JLabel exitButtonLabel = getExitButton();
+        add(exitButtonLabel,gbc);
+
+    }
+
+
+    private JLabel getExitButton() {
+        ImageIcon exitButtonImage = new ImageIcon(Commons.pathOfButton + "back.png");
+        ImageIcon onExitButtonImage = new ImageIcon(Commons.pathOfButton + "onBack.png");
+        JLabel exitButtonLabel = new JLabel(exitButtonImage);
+        exitButtonLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                buttonSound.getInstance().calAsync(pathOfAudio + "onButton.wav");
+                exitButtonLabel.setIcon(onExitButtonImage);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                exitButtonLabel.setIcon(exitButtonImage);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                var parent = (Breakout) SwingUtilities.getWindowAncestor(InfoScreen.this);
+                parent.cardLayout.show(parent.cardPanel, "start_screen");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                exitButtonLabel.setIcon(exitButtonImage);
+            }
+        });
+        return exitButtonLabel;
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Arka plan resmini çiz
+        ImageIcon background = new ImageIcon("src/resources/images/info.png");
+        Image backgroundImage = background.getImage();
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
