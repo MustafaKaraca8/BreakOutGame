c4ab98c9c63926683317bdf16e38a8581484af85
MustafaKaraca8 <mstafakarca@gmail.com>
2023-12-24 18:55:15 +0300

    Güncel

--- src/utility/Commons.java
+++ src/utility/Commons.java
@@ -4,14 +4,14 @@ public final class Commons {
 
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 750;
-    public static final int BOTTOM_EDGE = 740;
+    public static final int BOTTOM_EDGE = 760;
     // BUrayı değiştri amcık samet her levele özel brick sayısı olacak
     // İlk başa 0 koyma sebebimiz 0. level olmaması
     public static final int[] N_OF_BRICKS_PER_LEVEL = {0, 30, 25, 12, 8 , 0};
     public static final int INIT_PADDLE_X = 600;
     public static final int INIT_PADDLE_Y = 720;
-    public static final int INIT_BALL_X = 365;
-    public static final int INIT_BALL_Y = 570;
+    public static final int INIT_BALL_X = 600;
+    public static final int INIT_BALL_Y = 670;
     public static final int PERIOD = 10;
 
     public static final String pathOfButton = "src/resources/images/buttons/";
